c568b10f08fc414b7aa586c0ca49dfccc283b276
oofarhanafarhat <khnkaneez@gmail.com>
2025-07-15 15:08:01 +0500

    my first project in python

---
+++ streamlit_age_verifier.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title(" Age Verifier App")
+
+age_input = st.number_input("Enter your age:", min_value=-100, max_value=150, step=1)
+
+if st.button("Check Age"):
+    if age_input >= 60:
+        st.success("You are a senior citizen.")
+        st.info("You are respected and valuable for us. Please share your life experience and guide us.")
+    elif age_input >= 18:
+        st.success("You are an adult.")
+        st.info("Welcome! Make your life meaningful and responsible.")
+    elif age_input >= 0:
+        st.warning("You are a minor.")
+        st.info("Enjoy your childhood, but always keep learning!")
+    else:
+        st.error("Invalid age. Age cannot be negative.")
